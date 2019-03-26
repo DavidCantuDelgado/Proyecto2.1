64290ceef94ec64e35363d89d291163437546149
David Cantu <david.cantu.delgado@tec.mx>
2019-03-26 15:52:11 -0600

    Initial Commit

---
+++ Proyecto2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proyecto2.1
+//
+//  Created by David A Cantú Delgado on 3/26/19.
+//  Copyright © 2019 Tec de Monterrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
